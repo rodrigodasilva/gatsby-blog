3afa79f13eda4941430b43dc0ec4b34439d0c54d
rodrigodasilva <ribeiro.rodrigodasilva>
2020-12-07 20:19:28 -0300

    14. Renderizando dados com `StaticQuery`

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 const Profile = () => (
-  <div className="Profile-wrapper">
-    <h1>Rodrigo Ribeiro</h1>
-    <h2>Programador FullStack</h2>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita sed quod unde 
-      in nisi consectetur nobis ad molestiae distinctio.      
-    </p>
-  </div>  
+  <StaticQuery 
+    query={graphql`
+      query MyQuery {
+        site {
+          id
+          siteMetadata {
+            title
+            position
+            description
+          }
+        }
+      }    
+    `}
+    render={({ site: { siteMetadata: { title, position, description } } }) => (
+      <div className="Profile-wrapper">
+        <h1>{title}</h1>
+        <h2>{position}</h2>
+        <p>{description}</p>        
+      </div>  
+    )}
+  />
+
 )
 
 export default Profile
