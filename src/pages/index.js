ce1edad52360dc536a1599a4fee9476c14782cb0
rodrigodasilva <ribeiro.rodrigodasilva>
2021-04-08 19:56:20 -0300

    39. Adicionando fields com o `onCreateNode`

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,9 @@ const IndexPage = () => {
       allMarkdownRemark {
         edges {
           node {
+            fields {
+              slug
+            }
             frontmatter {
               background
               category
@@ -36,11 +39,12 @@ const IndexPage = () => {
           node: {
             frontmatter: { background, category, date, description, title },
             timeToRead,
+            fields: { slug },
           },
         }) => (
           <PostItem
             key={title}
-            slug="/about/"
+            slug={slug}
             background={background}
             category={category}
             date={date}
